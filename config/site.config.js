6ced171ca138144967862ab95641e783978fb5ec
Talaviya Bhavik <85677268+imnotdev25@users.noreply.github.com>
2024-02-25 21:23:09 +0530

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -31,14 +31,14 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double quotes - changing " to \".
   // You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-  'Powered by <a href="https://github.com/iRedScarf/onedrive-index" target="_blank" rel="noopener noreferrer">onedrive-index</a>.',
+  'Powered by <a href="https://github.com/" target="_blank" rel="noopener noreferrer">onedrive-index</a>.',
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name you provide. See the documentation for details.
   links: [
     {
       name: 'GitHub',
-      link: 'https://github.com/iRedScarf/onedrive-index',
+      link: 'https://github.com',
     },
   ],
 
